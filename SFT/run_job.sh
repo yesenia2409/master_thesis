42fe5945d5dd991f7802348308df5e702feb7b2c
Annica Skupch <annicaskupch@freenet.de>
2024-04-30 16:03:14 +0200

    add run job skript

---
+++ SFT/run_job.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --partition=single
+#SBATCH --ntasks=1
+#SBATCH --time=00:30:00
+#SBATCH --mem=80gb
+#SBATCH --gres=gpu:A40:1
+
+echo 'Running simulation'
+
+# Load conda
+module load devel/miniconda/3
+source $MINICONDA_HOME/etc/profile.d/conda.sh
+
+conda deactivate
+# Activate the conda environment
+conda activate thesis
+
+# activate CUDA
+module load devel/cuda/11.6
+
+# call python script
+python3 -u Selection_of_system_prompt/prompting_system_prompt.py
