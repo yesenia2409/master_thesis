ff28ae6a12a507ff56e3f0055e854e673617f261
Annica Skupch <annicaskupch@freenet.de>
2024-06-04 17:30:22 +0200

    update alignment with experts SFT

--- SFT/inference.py
+++ SFT/inference.py
@@ -47,7 +47,7 @@ def inference(model, tokenizer, prompts, labels, max_new_tokens):
     # inference
     with torch.no_grad():
         for prompt, label in zip(prompts, labels):
-            encode_dict = tokenizer(prompt, return_tensors="pt", padding=True, truncation=True)
+            encode_dict = tokenizer(prompt, return_tensors="pt", padding=True)
             txt_tokens = encode_dict["input_ids"].cuda()
             attention_mask = encode_dict["attention_mask"].cuda()
             kwargs = {"max_new_tokens": max_new_tokens, "eos_token_id": 50256, "pad_token_id": 50256}
