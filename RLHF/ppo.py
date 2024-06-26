3f290ed7cb6588334309ee14ab1449b379bd6f13
yesenia2409 <annicaskupch@freenet.de>
2024-06-26 17:18:29 +0200

    saving model

--- RLHF/ppo.py
+++ RLHF/ppo.py
@@ -1,12 +1,13 @@
+
 from datasets import Dataset
 import torch
 from peft import LoraConfig
-from reward_model import inference
+from reward_model import inference as reward_inference
 from tqdm import tqdm
 from transformers import AutoTokenizer, BitsAndBytesConfig
 from trl import AutoModelForCausalLMWithValueHead, PPOConfig, PPOTrainer, set_seed
 import pandas as pd
-from peft import AutoPeftModelForSequenceClassification
+from peft import AutoPeftModelForSequenceClassification, AutoPeftModelForCausalLM
 
 tqdm.pandas()
 MODEL_PATH = "../SFT/merged_model/SFT_for_expert_alignment/"
@@ -18,6 +19,8 @@ def build_dataset(dataset_path):
     train_set = pd.read_pickle(dataset_path)
     ppo_data = Dataset.from_pandas(train_set)
     ppo_data = ppo_data.remove_columns(['type', 'category', 'text', '__index_level_0__'])
+    ppo_data = ppo_data[:200]
+    ppo_data = Dataset.from_dict(ppo_data)
     ppo_data.set_format("pytorch")
     return ppo_data
 
@@ -28,7 +31,7 @@ def load_model_and_tokenizer():
         bnb_4bit_quant_type="nf4",
         bnb_4bit_compute_dtype=torch.float16,
     )
-
+    
     model = AutoModelForCausalLMWithValueHead.from_pretrained(
         pretrained_model_name_or_path=MODEL_PATH,
         trust_remote_code=True,
@@ -69,26 +72,24 @@ def collator(data):
 
 
 def inference(model, tokenizer, prompts, kwargs):
-    model.eval()
     query_tokens = []
     response_tokens = []
     preds = []
-    with torch.no_grad():
-        for prompt in prompts:
-            encode_dict = tokenizer(prompt, return_tensors="pt", padding=True)
-            txt_tokens = encode_dict["input_ids"].cuda()
-            summ_tokens = model.generate(input_ids=txt_tokens, **kwargs)
-            pred = tokenizer.batch_decode(summ_tokens)[0]
-
-            query_tokens.append(txt_tokens.squeeze())
-            response_tokens.append(summ_tokens.squeeze())
-            preds.append(pred)
-    model.train()
+    for prompt in prompts:
+        encode_dict = tokenizer(prompt, return_tensors="pt", padding=True)
+        txt_tokens = encode_dict["input_ids"].cuda()
+        summ_tokens = model.generate(input_ids=txt_tokens, **kwargs)
+        pred = tokenizer.batch_decode(summ_tokens)[0]
+        query_tokens.append(txt_tokens.squeeze())
+        response_tokens.append(summ_tokens.squeeze())
+        preds.append(pred)
     return query_tokens, response_tokens, preds
 
+
 def build_pipeline(ppo_config, ppo_trainer, policy_model, policy_tokenizer, reward_model, reward_tokenizer):
+    log_history = []
     kwargs = {
-        "max_new_tokens": 50,
+        "max_new_tokens": 100,
         "eos_token_id": 50256,
         "pad_token_id": 50256,
         # "top_p": 0.5,
@@ -104,24 +105,33 @@ def build_pipeline(ppo_config, ppo_trainer, policy_model, policy_tokenizer, rewa
             # Generate outputs
             query_tensors, response_tensors, pred = inference(policy_model, policy_tokenizer, batch["instruction"], kwargs)
             # print("Response Tensors: ", response_tensors)
-            # pred = pred.split("[EOS]")[1].split(ppo_trainer.tokenizer.eos_token)[0].split("[/EOS]")[0].replace("<|endoftext|>", "")
+            for idx in range(len(pred)):
+                pred[idx] = pred[idx].split("[/INST]")[1].split(policy_tokenizer.eos_token)[0]
             batch["response"] = pred
+            print(pred)
             
             # Compute rewards
             rewards_list = []
             for instr, resp in zip(batch["instruction"], batch["response"]):
                 print("Instr: ", instr)
                 print("Resp: ", resp)
-                reward = inference(reward_tokenizer, reward_model, resp)
+                reward = reward_inference(reward_tokenizer, reward_model, resp)
                 rewards_list.append(torch.tensor(reward).to(DEVICE))
             # print("Rewards List: ", rewards_list)
 
             # Run PPO step
             stats = ppo_trainer.step(query_tensors, response_tensors, rewards_list)
-            print("Stats: ", stats)
+            # print("Stats: ", stats)
             ppo_trainer.log_stats(stats, batch, rewards_list)
+            log_history.append(stats)
+            print("LOSS: ", stats['ppo/loss/value'], stats['ppo/loss/total'])
             batch_counter += 1
 
+    with open("Output_files/slurm_files/trainer_log_history_1epoch_2_00E-5Lr_2batch.txt", "a") as text_file:
+        text_file.write(str(log_history))
+    
+    policy_model.save_pretrained("Policy_Model/", save_serialisation=True)
+    print("Done saving model!")  
 
 if __name__ == "__main__":
 
@@ -160,3 +170,4 @@ if __name__ == "__main__":
     )
 
     build_pipeline(ppo_config, ppo_trainer, policy_model, policy_tokenizer, reward_model, reward_tokenizer)
+    
